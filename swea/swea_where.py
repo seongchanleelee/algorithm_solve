3bdcebe4103236be582c9d633c586e0eafb5bc6d
leeseongchan <gotls0001@naver.com>
2023-01-22 16:13:33 +0900

    # solve swea

--- swea/swea_where.py
+++ swea/swea_where.py
@@ -3,37 +3,32 @@ for tc in range(1,t+1):
     n,k = list(map(int,input().split()))
     arr = [list(map(int,input().split()))for _ in range(n)]
     answer = 0
-    # 가로로 확인
-    cnt = 0
-    for y in range(len(arr)):
-        if cnt ==k:
-            answer +=1
-            cnt = 0
-        for x in range(len(arr[y])):
-            if arr[y][x] ==1:
-                cnt+=1
-            else:
+
+    # 가로로 탐색
+    for y in range(n):
+        cnt = 0
+        for x in range(n):
+            if arr[y][x] ==0:
                 if cnt ==k:
                     answer +=1
-
-                cnt = 0
-            if cnt == k:
-                answer +=1
-                cnt = 0
-    cnt = 0
-    for x in range(len(arr[0])):
-        if cnt ==k:
-            answer +=1
-            cnt = 0
-        for y in range(len(arr)):
-            if arr[y][x] ==1:
+                cnt =0
+                continue
+            else:
                 cnt +=1
-            else:
+        if cnt ==k:
+            answer +=1
+
+    #세로로 탐색
+    for x in range(n):
+        cnt = 0
+        for y in range(n):
+            if arr[y][x] ==0:
                 if cnt ==k:
                     answer +=1
                 cnt = 0
-            # if cnt ==k:
-            #     answer +=1
-            #     cnt =0
-
+                continue
+            else:
+                cnt +=1
+        if cnt ==k:
+            answer +=1
     print(f"#{tc} {answer}")
