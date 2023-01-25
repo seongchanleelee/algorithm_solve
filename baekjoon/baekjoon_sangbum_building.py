2d6bb0f9e8fc1340e1e54cb020a6cfde9a433dc6
leeseongchan <gotls0001@naver.com>
2023-01-25 20:44:04 +0900

    solve baekjoon

--- baekjoon/baekjoon_sangbum_building.py
+++ baekjoon/baekjoon_sangbum_building.py
@@ -4,22 +4,29 @@ while 1:
     building = []
     visit = []
     if l==0 and r==0 and c ==0:
-        break
+        sys.exit()
     for i in range(l):
-        arr = [list(input())for _ in range(r)]
+        arr = []
+        for _ in range(r+1):
+            a = list(input())
+            if a ==[]: continue
+            arr.append(a)
+
         arr2 = [[0]*c for _ in range(r)]
+
         building.append(arr)
         visit.append(arr2)
-
     def bfs(start):
+        global result
         q = []
         q.append(start)
 
         while q:
             nowf, nowy, nowx, nowcnt = q.pop(0)
-
-            if arr[nowf][nowy][nowx] == "E":
-                return nowcnt
+            if building[nowf][nowy][nowx] == "E":
+                # result = nowcnt
+                print(f"Escaped in {nowcnt} minute(s).")
+                return
             directy = [-1,1,0,0,0,0] # 상 하 좌 우 위 아래
             directx = [0,0,-1,1,0,0]
             directf = [0,0,0,0,-1,1]
@@ -34,12 +41,16 @@ while 1:
                     if building[df][dy][dx] =="#": continue
                     visit[df][dy][dx] = 1
                     q.append((df, dy, dx, nowcnt+1))
+        print("Trapped!")
 
-
+    result = None
     for f in range(l):
         for y in range(r):
             for x in range(l):
                 if building[f][y][x] =="S":
-                    print(bfs((f,y,x,0)))
-
-
+                    visit[f][y][x] = 1
+                    bfs((f,y,x,0))
+                    # if result ==None:
+                    #     print("Trapped!")
+                    # else:
+                    #     print(f"Escaped in {result} minute(s).")
